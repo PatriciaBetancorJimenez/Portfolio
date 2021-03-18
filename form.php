294ef468bbb2e2d98e6336d0c54ea5627e4fbfa6
Patricia <patricia@Patricias-MacBook-Air.local>
2021-03-18 16:13:47 +0000

    php test 6

--- form.php
+++ form.php
@@ -11,13 +11,11 @@
 	$email_body = "You have received a new message from the user $name.\n".
                             "Here is the message:\n $message".
 
-  $to = "patriciabetancorjimenez";
+  $to = "patriciabetancorjimenez@gmail.com";
 
-  $headers = "From: $email_from \r\n";
+  
 
-  $headers .= "Reply-To: $visitor_email \r\n";
-
-  mail($to,$email_subject,$email_body,$headers);
+  mail($to,$email_subject,$email_body);
 
 
 ?>
