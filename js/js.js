0ce0cf23336fab97d70bd61666b6ec9a3cdfb437
Patricia <patricia@Patricias-MacBook-Air.local>
2021-03-18 13:01:37 +0000

    added responsive nav

--- js/js.js
+++ js/js.js
@@ -11,3 +11,16 @@ $('a[href*="#"]').on("click", function (event) {
     $("html, body").animate({ scrollTop: $(hash).offset().top - 140 }, 800);
   }
 });
+
+let burgerButton = document.querySelector(".nav__burger");
+let navList = document.querySelector(".nav__list");
+
+burgerButton.addEventListener("click", function () {
+  navList.classList.toggle("nav__mobile");
+});
+
+window.addEventListener("resize", function () {
+  if (innerWidth > 900) {
+    navList.classList.remove("nav__mobile");
+  }
+});
