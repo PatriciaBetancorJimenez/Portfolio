2c80e5c9837e528d724b431569001aea612b547c
Patricia <patricia@Patricias-MacBook-Air.local>
2021-04-02 17:43:01 +0100

    phpMailer

--- form.php
+++ form.php
@@ -1,19 +1,73 @@
-<?php
+<?php 
+
+
+$result="";
 
 
 if(isset($_POST['submit'])){
-    $name=$_POST['name'];
-    $mailFrom=$_POST['email'];
-    $message=$_POST['message'];
 
 
+require 'phpMailer/PHPMailerAutoload.php';
 
-    $mailTo="brujix_999@hotmail.com";
-    $header="From: ".$mailFrom;
-    $txt="You have received an email from ".$name.".\n\n".$message;
 
-    mail($mailTo,$header,$txt);
-    header("Location: index.html?mailsend");
+$mail = new PHPMailer;
+
+
+$mail->Host='smtp.gmail.com';
+
+
+$mail->Port=587;
+
+
+$mail->SMTPAuth=true;
+
+
+$mail->SMTPSecure='tls';
+
+
+$mail->Username='patriciabetancorjimenez@gmail.com';
+
+
+$mail->Password='';
+
+
+$mail->setFrom($_POST['email'],$_POST['name']);
+
+
+$mail->addAddress('patriciabetancorjimenez@gmail.com');
+
+
+$mail->addReplyTo($_POST['email'],$_POST['name']);
+
+
+$mail->isHTML(true);
+
+
+$mail->Subject='Form Submission Portfolio:' ;
+
+
+$mail->Body='<h3>Name :'.$_POST['name'].'<br> Email: '.$_POST['email'].'<br>Message: '.$_POST['message'].'</h3>';
+
+
+if(!$mail->send()){
+
+
+$result = "Something went worng. Please try again.";
+
+
 }
+
+
+else {
+
+
+    $result="Thanks" .$_POST['name']. " for contacting us. We'll get back to you soon!";
+
+
+  }
+
+
+}
+
 
 ?>
