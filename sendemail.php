ae661e47387ea446076d78aafdaa52b1b6e65567
PatriciaBetancorJimenez <78312438+PatriciaBetancorJimenez@users.noreply.github.com>
2021-06-22 13:16:31 +0100

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -18,13 +18,13 @@ if(isset($_POST['submit'])){
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'patriciabetancorjimenez@gmail.com'; // Gmail address which you want to use as SMTP server
-    $mail->Password = 'Dionisio_999?'; // Gmail address Password
+    $mail->Username = ''; // Gmail address which you want to use as SMTP server
+    $mail->Password = ''; // Gmail address Password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = '587';
 
-    $mail->setFrom('patriciabetancorjimenez@gmail.com'); // Gmail address which you used as SMTP server
-    $mail->addAddress('patriciabetancorjimenez@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
+    $mail->setFrom(''); // Gmail address which you used as SMTP server
+    $mail->addAddress(''); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
 
     $mail->isHTML(true);
     $mail->Subject = 'Message Received (Contact Page)';
